01875ffac73e45dd7c52ef1396376cf0b0bf430d
Olha Lavoryk <olhalavoryk@gmail.com>
2023-09-12 10:04:06 +0200

    Working prototype of correctionlib from workspace production

--- create_crosspog_json.py
+++ create_crosspog_json.py
@@ -187,10 +187,7 @@ class pt_eta_correction(Correction):
                         "input": "abs(eta)",
                         "edges": self.etabinning,
                         "flow": "clamp",
-                        # "content": self.get_sfs_for_etas(
-                        #     pt, self.etabinning, self.data_only
-                        # ),
-                        "content": [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1], 
+                        "content" : self.workspace.get_emb_id_sel( pt, self.etabinning)
                     }
                     for pt in self.ptbinning[:-1]
                 ],
@@ -215,12 +212,11 @@ class pt_eta_correction(Correction):
                                 "content": [
                                     {
                                         "key": "mc",
-                                        # "value": 0.777,                    #it writes mc sf
-                                        "value": self.workspace.get_sfs(pt, eta, self.name ,"mc")
+                                        "value": self.workspace.get_sfs_2D( pt, eta, self.name, "mc")
                                     },
                                     {
                                         "key": "emb",
-                                        "value": self.workspace.get_sfs(pt, eta, self.name ,"emb"),                    #it writes emb sf
+                                        "value": self.workspace.get_sfs_2D( pt, eta, self.name, "emb")
                                     },
                                 ],
                             }
@@ -253,207 +249,219 @@ class pt_eta_correction(Correction):
         JSONEncoder.write(self.correction, self.fname)
 
 
-# class emb_doublemuon_correction(Correction):
-#     def __init__(
-#         self,
-#         tag,
-#         name,
-#         configfile,
-#         era,
-#         outdir,
-#         triggernames,
-#         fname="",
-#         data_only=True,
-#         verbose=False,
-#     ):
-#         super(emb_doublemuon_correction, self).__init__(
-#             tag,
-#             name,
-#             outdir,
-#             configfile,
-#             era,
-#             fname,
-#             data_only,
-#             verbose,
-#         )
-#         self.types = ["Data"]
-#         self.names = triggernames
-
-#     def parse_config(self):
-#         config = yaml.safe_load(open(self.configfile))
-#         # self.ptbinning = config[self.name]["bins_x"]
-#         # self.etabinning = config[self.name]["bins_y"]
+class emb_doublemuon_correction(Correction):
+    def __init__(
+        self,
+        tag,
+        name,
+        configfile,
+        era,
+        outdir,
+        triggernames,
+        fname="",
+        data_only=True,
+        verbose=False,
+    ):
+        super(emb_doublemuon_correction, self).__init__(
+            tag,
+            name,
+            outdir,
+            configfile,
+            era,
+            fname,
+            data_only,
+            verbose,
+        )
+        self.types = ["Data"]
+        self.names = triggernames
 
 
-#         self.ptbinning = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 31, 34, 37, 40, 45, 50, 60, 70, 100, 1000]   # you should read binning from config file
-#         self.etabinning = [0, 0.1, 0.3, 0.8, 1.0, 1.2, 1.6, 1.8, 2.1, 2.4]                                # analogous to pt binning
+    def set_workspace(self):
+        self.workspace = WorkspaceReader("/work/olavoryk/corr_lib_com/htt_scalefactors_UL_"+str(self.era).replace("UL","")+".root")
 
-#         basename = (
-#             str(os.path.basename(self.configfile)).split("_")[1].replace(".yaml", "")
-#         )
-
-#         self.info = config[self.name]["info"]
-#         self.header = config[self.name]["header"]
-
-#         print("\n info: ", self.info)
-#         print("header: ", self.header)
+    def parse_config(self):
+        config = yaml.safe_load(open(self.configfile))
+        self.ptbinning = config[self.name]["bins_x"]
+        self.etabinning = config[self.name]["bins_y"]
 
 
-#     def setup_scheme(self):
-#         self.correctionset = {
-#             "version": 0,
-#             "name": self.name,
-#             "description": self.info,
-#             "inputs": [
-#                 {
-#                     "name": "pt_1",
-#                     "type": "real",
-#                     "description": "Reconstructed leading genparticle pT",
-#                 },
-#                 {
-#                     "name": "abs(eta_1)",
-#                     "type": "real",
-#                     "description": "Reconstructed leading genparticle eta",
-#                 },
-#                 {
-#                     "name": "pt_2",
-#                     "type": "real",
-#                     "description": "Reconstructed trailing genparticle pT",
-#                 },
-#                 {
-#                     "name": "abs(eta_2)",
-#                     "type": "real",
-#                     "description": "Reconstructed trailing genparticle eta",
-#                 },
-#             ],
-#             "output": {
-#                 "name": "sf",
-#                 "type": "real",
-#                 "description": "pT-eta-dependent scale factor",
-#             },
-#             "data": None,
-#         }
-#         if not self.data_only:
-#             self.correctionset["inputs"].append(
-#                 {
-#                     "name": "type",
-#                     "type": "string",
-#                     "description": "Type of correction: Embedding or MC",
-#                 }
-#             )
+        basename = (
+            str(os.path.basename(self.configfile)).split("_")[1].replace(".yaml", "")
+        )
 
-#     def generate_sfs(self):
-#         if self.data_only:
-#             sfs = schema.Binning.parse_obj(
-#                 {
-#                     "nodetype": "binning",
-#                     "input": "pt_1",
-#                     "edges": self.ptbinning,
-#                     "flow": "clamp",
-#                     "content": [
-#                         {
-#                             "nodetype": "binning",
-#                             "input": "abs(eta_1)",
-#                             "edges": self.etabinning,
-#                             "flow": "clamp",
-#                             "content": [
-#                                 {
-#                                     "nodetype": "binning",
-#                                     "input": "pt_2",
-#                                     "edges": self.ptbinning,
-#                                     "flow": "clamp",
-#                                     "content": [
-#                                         {
-#                                             "nodetype": "binning",
-#                                             "input": "abs(eta_2)",
-#                                             "edges": self.etabinning,
-#                                             "flow": "clamp",
-#                                             # "content": self.get_sf(
-#                                             #     pt_1,
-#                                             #     eta_1,
-#                                             #     pt_2,
-#                                             #     self.etabinning,
-#                                             #     self.data_only,
-#                                             # ),
-#                                             "content": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ],
-#                                         }
-#                                         for pt_2 in self.ptbinning[:-1]
-#                                     ],
-#                                 }
-#                                 for eta_1 in self.etabinning[:-1]
-#                             ],
-#                         }
-#                         for pt_1 in self.ptbinning[:-1]
-#                     ],
-#                 }
-#             )
-#         else:
-#             raise Exception("Not implemented")
-#         return sfs
+        self.info = config[self.name]["info"]
+        self.header = config[self.name]["header"]
 
-#     def get_sf(self, pt_1, eta_1, pt_2, eta_binning, data_only, inputtype=None):
-#         sfs = []
-#         # leave of the last eta bin, which is the overflow bin
-#         for eta_2 in eta_binning[:-1]:
-#             efficiency = {}
-#             for lepton in [1, 2]:
-#                 for trigger in self.names:
-#                     shortname = "17" if "17" in trigger else "8"
-#                     if lepton == 1:
-#                         pt_ = pt_1
-#                         eta_ = eta_1
-#                     else:
-#                         pt_ = pt_2
-#                         eta_ = eta_2
-#                     efficiency["{}_{}".format(shortname, lepton)] = self.inputobjects[
-#                         trigger
-#                     ]["object"].GetBinContent(
-#                         self.inputobjects[trigger]["object"].GetXaxis().FindBin(pt_),
-#                         self.inputobjects[trigger]["object"].GetYaxis().FindBin(eta_),
-#                     )
-#             sf = 1.0 / (
-#                 efficiency["8_1"] * efficiency["17_2"]
-#                 + efficiency["8_2"] * efficiency["17_1"]
-#                 - efficiency["17_1"] * efficiency["17_2"]
-#             )
-#             # sanitize if all efficiencies are close to zero
-#             if (
-#                 abs(
-#                     efficiency["8_1"] * efficiency["17_2"]
-#                     + efficiency["8_2"] * efficiency["17_1"]
-#                     - efficiency["17_1"] * efficiency["17_2"]
-#                 )
-#                 < epsilon
-#             ):
-#                 print(
-#                     "Sanitizing SF for pt_1 = {}, eta_1 = {}, pt_2 = {}, eta_2 = {}".format(
-#                         pt_1, eta_1, pt_2, eta_2
-#                     )
-#                 )
-#                 print("calefactor before: {}".format(sf))
-#                 print("Scalefactor after: ", 1.0)
-#                 sf = 0.0
-#             sfs.append(sf)
-#             if self.verbose:
-#                 print("pt_1:", pt_1, "eta_1:", eta_1, "pt_2:", pt_2, "eta_2:", eta_2)
-#                 print("sf:", sf)
-#         return sfs
 
-#     def generate_scheme(self):
-#         self.parse_config()
-#         self.setup_scheme()
-#         self.correctionset["data"] = self.generate_sfs()
-#         output_corr = schema.Correction.parse_obj(self.correctionset)
-#         self.correction = output_corr
+    def setup_scheme(self):
+        self.correctionset = {
+            "version": 0,
+            "name": self.name,
+            "description": self.info,
+            "inputs": [
+                {
+                    "name": "pt_1",
+                    "type": "real",
+                    "description": "Reconstructed leading genparticle pT",
+                },
+                {
+                    "name": "abs(eta_1)",
+                    "type": "real",
+                    "description": "Reconstructed leading genparticle eta",
+                },
+                {
+                    "name": "pt_2",
+                    "type": "real",
+                    "description": "Reconstructed trailing genparticle pT",
+                },
+                {
+                    "name": "abs(eta_2)",
+                    "type": "real",
+                    "description": "Reconstructed trailing genparticle eta",
+                },
+            ],
+            "output": {
+                "name": "sf",
+                "type": "real",
+                "description": "pT-eta-dependent scale factor",
+            },
+            "data": None,
+        }
+        if not self.data_only:
+            self.correctionset["inputs"].append(
+                {
+                    "name": "type",
+                    "type": "string",
+                    "description": "Type of correction: Embedding or MC",
+                }
+            )
 
-#     def write_scheme(self):
-#         if self.verbose >= 2:
-#             print(JSONEncoder.dumps(self.correction))
-#         elif self.verbose >= 1:
-#             print(self.correction)
-#         if self.fname:
-#             print(f">>> Writing {self.fname}...")
-#         JSONEncoder.write(self.correction, self.fname)
+    def generate_sfs(self):
+        if self.data_only:
+            sfs = schema.Binning.parse_obj(
+                {
+                    "nodetype": "binning",
+                    "input": "pt_1",
+                    "edges": self.ptbinning,
+                    "flow": "clamp",
+                    "content": [
+                        {
+                            "nodetype": "binning",
+                            "input": "abs(eta_1)",
+                            "edges": self.etabinning,
+                            "flow": "clamp",
+                            "content": [
+                                {
+                                    "nodetype": "binning",
+                                    "input": "pt_2",
+                                    "edges": self.ptbinning,
+                                    "flow": "clamp",
+                                    "content": [
+                                        {
+                                            "nodetype": "binning",
+                                            "input": "abs(eta_2)",
+                                            "edges": self.etabinning,
+                                            "flow": "clamp",
+                                            # "content": self.get_sf(
+                                            #     pt_1,
+                                            #     eta_1,
+                                            #     pt_2,
+                                            #     self.etabinning,
+                                            #     self.data_only,
+                                            # ),
+
+                                            "content" : self.workspace.get_emb_sel_sfs( pt_1, eta_1, pt_2, self.etabinning)
+                                            
+                                        }
+                                        for pt_2 in self.ptbinning[:-1]
+                                    ],
+                                }
+                                for eta_1 in self.etabinning[:-1]
+                            ],
+                        }
+                        for pt_1 in self.ptbinning[:-1]
+                    ],
+                }
+            )
+        else:
+            raise Exception("Not implemented")
+        
+        return sfs
+
+
+    # def get_sf(self, pt_1, eta_1, pt_2, eta_binning, data_only, inputtype=None):
+    #     sfs = []
+    #     # leave of the last eta bin, which is the overflow bin
+    #     for eta_2 in eta_binning[:-1]:
+    #         efficiency = {}
+    #         for lepton in [1, 2]:
+    #             for trigger in self.names:
+    #                 shortname = "17" if "17" in trigger else "8"
+    #                 if lepton == 1:
+    #                     pt_ = pt_1
+    #                     eta_ = eta_1
+    #                 else:
+    #                     pt_ = pt_2
+    #                     eta_ = eta_2
+    #                 # efficiency["{}_{}".format(shortname, lepton)] = self.inputobjects[
+    #                 #     trigger
+    #                 # ]["object"].GetBinContent(
+    #                 #     self.inputobjects[trigger]["object"].GetXaxis().FindBin(pt_),
+    #                 #     self.inputobjects[trigger]["object"].GetYaxis().FindBin(eta_),
+    #                 # )
+
+    #                 if trigger == "Trg17_pt_eta_bins":
+
+    #                     efficiency["{}_{}".format(shortname, lepton)] = 0.95
+    #                 elif trigger == "Trg8_pt_eta_bins":
+    #                     efficiency["{}_{}".format(shortname, lepton)] = 0.88
+
+    #         sf = 1.0 / (
+    #             efficiency["8_1"] * efficiency["17_2"]
+    #             + efficiency["8_2"] * efficiency["17_1"]
+    #             - efficiency["17_1"] * efficiency["17_2"]
+    #         )
+    #         # sanitize if all efficiencies are close to zero
+    #         if (
+    #             abs(
+    #                 efficiency["8_1"] * efficiency["17_2"]
+    #                 + efficiency["8_2"] * efficiency["17_1"]
+    #                 - efficiency["17_1"] * efficiency["17_2"]
+    #             )
+    #             < epsilon
+    #         ):
+    #             print(
+    #                 "Sanitizing SF for pt_1 = {}, eta_1 = {}, pt_2 = {}, eta_2 = {}".format(
+    #                     pt_1, eta_1, pt_2, eta_2
+    #                 )
+    #             )
+    #             print("calefactor before: {}".format(sf))
+    #             print("Scalefactor after: ", 1.0)
+    #             sf = 0.0
+    #         sfs.append(sf)
+    #         # if self.verbose:
+    #     # print("\n pt_1:", pt_1, "eta_1:", eta_1, "pt_2:", pt_2, "eta_2:", eta_2)
+    #     # print("sf:", sf)
+
+    #     # print("\n sfs: ", sfs)
+    #     return sfs
+
+    def generate_scheme(self):
+        self.parse_config()
+        self.setup_scheme()
+        self.set_workspace()
+        self.correctionset["data"] = self.generate_sfs()
+        output_corr = schema.Correction.parse_obj(self.correctionset)
+        self.correction = output_corr
+
+    def write_scheme(self):
+        if self.verbose >= 2:
+            print(JSONEncoder.dumps(self.correction))
+        elif self.verbose >= 1:
+            print(self.correction)
+        if self.fname:
+            print(f">>> Writing {self.fname}...")
+        JSONEncoder.write(self.correction, self.fname)
 
 
 if __name__ == "__main__":
--- workspace_reader.py
+++ workspace_reader.py
@@ -1,6 +1,5 @@
 import ROOT 
 
-file = "htt_scalefactors_UL_2016postVFP.root"
 
 func_dictionary = {
 
@@ -36,48 +35,25 @@ class WorkspaceReader:
         self.ws = ws_file.Get("w")
 
 
-
-    def get_sfs(self, pt, eta, name, emb_mc_flag):
-        try: 
-
-            argset = self.ws.argSet("m_pt,m_eta")
-            argset.setRealValue("m_pt", pt)
-            argset.setRealValue("m_eta", eta)
-
-            if name in func_dictionary.keys():
-
-                if emb_mc_flag == "emb":
-                    roofunction = self.ws.function(func_dictionary[name][0])
-
-                elif emb_mc_flag == "mc":
-                    roofunction = self.ws.function(func_dictionary[name][1])
-
-                result = roofunction.getVal(argset)
-
-                # result = round(result, 5)  # round to 5 digits
-
-                # print ("\n the following function: {} have a scale factor in the following bin: pt = {}, eta = {} is returned a corr factor = {}".format(name, pt, eta, result))
-
-                return result
-            elif name not in func_dictionary.keys():
-                print("\n the following function: {} is not in the dictionary. 1. is returned".format(name))
-                return 1.0
-
-        except ReferenceError:
-            print("\ the following function: {} have a scale factor in the following pT-eta {}-{}bin. 1. is returned".format(name, pt, eta))
-            return 1.0
         
     def get_sfs_2D(self, pt, eta, name, emb_mc_flag):
         if name in func_dictionary.keys():
 
-            if emb_mc_flag == "emb":
+            result = 1.0
 
-                hist = self.ws.obj("hist_"+func_dictionary[name][0])
+            if pt > 200.0:
+                return result
 
-            elif emb_mc_flag == "mc":
-                hist = self.ws.obj("hist_"+func_dictionary[name][1])
+            if pt < 200.0:
 
-            result = hist.GetBinContent( hist.GetXaxis().FindBin(pt) , hist.GetYaxis().FindBin(eta) )
+                if emb_mc_flag == "emb":
+
+                    hist = self.ws.obj("hist_"+func_dictionary[name][0])
+
+                elif emb_mc_flag == "mc":
+                    hist = self.ws.obj("hist_"+func_dictionary[name][1])
+
+                result = hist.GetBinContent( hist.GetXaxis().FindBin(pt) , hist.GetYaxis().FindBin(eta) )
         
             return result
 
@@ -86,70 +62,53 @@ class WorkspaceReader:
             return 1.0
 
 
-        
-            
-    def get_emb_sel_sfs(self, pt_1, eta_1, pt_2, eta_binning):
 
+    def get_emb_sel_sfs(self, pt_1, eta_1, pt_2, eta_binning):
         sfs = []
 
-        for eta_2 in eta_binning:
+        for eta_2 in eta_binning[:-1]:
+            argset = self.ws.argSet("gt1_pt,gt1_eta,gt2_pt,gt2_eta")
+
+            argset.setRealValue("gt1_pt", pt_1 )
+            argset.setRealValue("gt1_eta", eta_1)
 
 
-            try: 
+            argset.setRealValue("gt2_pt", pt_2)
+            argset.setRealValue("gt2_eta", eta_2)
 
+            roofunction = self.ws.function("m_sel_trg_ic_data")
 
-                argset_17_1 = self.ws.argSet("m_pt,m_eta")
-                argset_17_2 = self.ws.argSet("m_pt,m_eta")
+            efficiency = roofunction.getVal(argset)
+            efficiency = min(1./efficiency, 20) 
 
-                argset_8_1 = self.ws.argSet("m_pt,m_eta")
-                argset_8_2 = self.ws.argSet("m_pt,m_eta")
+            sfs.append(efficiency)
 
+        return sfs
+    
 
-                argset_17_1.setRealValue("m_pt", pt_1)
-                argset_17_1.setRealValue("m_eta", eta_1)
+    def get_emb_id_sel(self, pt,  eta_binning):
+        sfs = []
 
+        for eta in eta_binning[:-1]:
+            argset = self.ws.argSet("gt_pt,gt_eta")
 
-                argset_17_2.setRealValue("m_pt", pt_1)
-                argset_17_2.setRealValue("m_eta", eta_1)
+            argset.setRealValue("gt_pt", pt )
+            argset.setRealValue("gt_eta", eta)
 
+            roofunction = self.ws.function("m_sel_id_ic_data")
 
-                argset_8_1.setRealValue("m_pt", pt_1)
-                argset_8_1.setRealValue("m_eta", eta_1)
+            efficiency = roofunction.getVal(argset)
+            efficiency = min(1./efficiency, 20) 
 
+            sfs.append(efficiency)
 
-                argset_8_2.setRealValue("m_pt", pt_2)
-                argset_8_2.setRealValue("m_eta", eta_2)
-
-
-                roofunction_17_1 = self.ws.function("m_sel_trg_17_ic_1_data")
-                roofunction_17_2 = self.ws.function("m_sel_trg_17_ic_2_data")
-
-                roofunction_8_1 = self.ws.function("m_sel_trg_8_ic_1_data")
-                roofunction_8_2 = self.ws.function("m_sel_trg_8_ic_2_data")
-
-                efficiency_17_1 = roofunction_17_1.getVal(argset_17_1)
-                efficiency_17_2 = roofunction_17_2.getVal(argset_17_2)
-
-                efficiency_8_1 = roofunction_8_1.getVal(argset_8_1)
-
-                efficiency_8_2 = roofunction_8_2.getVal(argset_8_2)
-
-                print("\n efficiency 17_1 = {}, efficiency 17_2 = {}, efficiency 8_1 = {}, efficiency 8_2 = {}".format(efficiency_17_1, efficiency_17_2, efficiency_8_1, efficiency_8_2))
-
-
-                result = 1 / ( efficiency_17_1 * efficiency_8_2 + efficiency_8_1 * efficiency_17_2 - efficiency_17_1 * efficiency_17_2)
-
-                print(result)
+        return sfs
 
 
 
-                # return result
-                sfs.append(result)
 
 
-            except ReferenceError:
-                # print("\ the following function: {} have a scale factor in the following pT-eta {}-{}bin. 1. is returned".format(name, pt, eta))
-                # return 1.0
-                sfs.append(1.0)
-        return [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ]
-        
+
+
+            
+    
