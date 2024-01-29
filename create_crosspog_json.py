1aade501b5de7cbbddcd961e36a250bbaea68df0
Artur Monsch <60860160+a-monsch@users.noreply.github.com>
2024-01-29 14:07:24 +0100

    Introducing workspace-file argument and fixing path issues, reformatting.

--- create_crosspog_json.py
+++ create_crosspog_json.py
@@ -1,11 +1,13 @@
-import correctionlib._core as core
-import correctionlib.schemav2 as schema
-import correctionlib.JSONEncoder as JSONEncoder
-from workspace_reader import WorkspaceReader
-import ROOT
 import json
 import os
+
+import correctionlib._core as core
+import correctionlib.JSONEncoder as JSONEncoder
+import correctionlib.schemav2 as schema
+import ROOT
 import yaml
+
+from workspace_reader import WorkspaceReader
 
 # set epsilon value to avoid giant scale factors
 epsilon = 0.0001
@@ -28,17 +30,11 @@ class CorrectionSet(object):
         elif isinstance(correction, Correction):
             self.corrections.append(correction.correctionset)
         else:
-            raise TypeError(
-                "Correction must be a Correction object or a dictionary, not {}".format(
-                    type(correction)
-                )
-            )
+            raise TypeError("Correction must be a Correction object or a dictionary, not {}".format(type(correction)))
 
     def write_json(self, outputfile):
         # Create the JSON object
-        cset = schema.CorrectionSet(
-            schema_version=schema.VERSION, corrections=self.corrections
-        )
+        cset = schema.CorrectionSet(schema_version=schema.VERSION, corrections=self.corrections)
         print(f">>> Writing {outputfile}...")
         JSONEncoder.write(cset, outputfile)
         JSONEncoder.write(cset, outputfile + ".gz")
@@ -118,11 +114,10 @@ class pt_eta_correction(Correction):
         if self.data_only:
             self.types = ["Data"]
 
-    
-    def set_workspace(self):
-        self.workspace = WorkspaceReader("/work/olavoryk/corr_lib_com/htt_scalefactors_UL_"+str(self.era).replace("UL","")+".root")
-
-
+    def set_workspace(self, file=None):
+        if file is None:
+            file = "./htt_scalefactors_UL_" + str(self.era).replace("UL", "") + ".root"
+        self.workspace = WorkspaceReader(file)
 
     def parse_config(self):
         config = yaml.safe_load(open(self.configfile))
@@ -131,7 +126,6 @@ class pt_eta_correction(Correction):
         basename = str(os.path.basename(self.configfile)).split("_")[1]
         self.info = config[self.name]["info"]
         self.header = config[self.name]["header"]
-
 
     def GetFromTFile(self, inputfile, object):
         print("Getting ", object, "from ", inputfile)
@@ -187,7 +181,7 @@ class pt_eta_correction(Correction):
                         "input": "abs(eta)",
                         "edges": self.etabinning,
                         "flow": "clamp",
-                        "content" : self.workspace.get_emb_id_sel( pt, self.etabinning)
+                        "content": self.workspace.get_emb_id_sel(pt, self.etabinning),
                     }
                     for pt in self.ptbinning[:-1]
                 ],
@@ -210,14 +204,8 @@ class pt_eta_correction(Correction):
                                 "nodetype": "category",
                                 "input": "type",
                                 "content": [
-                                    {
-                                        "key": "mc",
-                                        "value": self.workspace.get_sfs_2D( pt, eta, self.name, "mc")
-                                    },
-                                    {
-                                        "key": "emb",
-                                        "value": self.workspace.get_sfs_2D( pt, eta, self.name, "emb")
-                                    },
+                                    {"key": "mc", "value": self.workspace.get_sfs_2D(pt, eta, self.name, "mc")},
+                                    {"key": "emb", "value": self.workspace.get_sfs_2D(pt, eta, self.name, "emb")},
                                 ],
                             }
                             for eta in self.etabinning[:-1]
@@ -228,12 +216,10 @@ class pt_eta_correction(Correction):
             }
         return schema.Binning.parse_obj(sfs)
 
- 
-
-    def generate_scheme(self):
+    def generate_scheme(self, workspace_file=None):
         self.parse_config()
         self.setup_scheme()
-        self.set_workspace()
+        self.set_workspace(workspace_file)
         self.correctionset["data"] = self.generate_sfs()
         output_corr = schema.Correction.parse_obj(self.correctionset)
         self.correction = output_corr
@@ -275,23 +261,20 @@ class emb_doublemuon_correction(Correction):
         self.types = ["Data"]
         self.names = triggernames
 
-
-    def set_workspace(self):
-        self.workspace = WorkspaceReader("/work/olavoryk/corr_lib_com/htt_scalefactors_UL_"+str(self.era).replace("UL","")+".root")
+    def set_workspace(self, file=None):
+        if file is None:
+            file = "./htt_scalefactors_UL_" + str(self.era).replace("UL", "") + ".root"
+        self.workspace = WorkspaceReader(file)
 
     def parse_config(self):
         config = yaml.safe_load(open(self.configfile))
         self.ptbinning = config[self.name]["bins_x"]
         self.etabinning = config[self.name]["bins_y"]
 
-
-        basename = (
-            str(os.path.basename(self.configfile)).split("_")[1].replace(".yaml", "")
-        )
+        basename = str(os.path.basename(self.configfile)).split("_")[1].replace(".yaml", "")
 
         self.info = config[self.name]["info"]
         self.header = config[self.name]["header"]
-
 
     def setup_scheme(self):
         self.correctionset = {
@@ -362,8 +345,7 @@ class emb_doublemuon_correction(Correction):
                                             "input": "abs(eta_2)",
                                             "edges": self.etabinning,
                                             "flow": "clamp",
-                                            "content" : self.workspace.get_emb_sel_sfs( pt_1, eta_1, pt_2, self.etabinning)
-                                            
+                                            "content": self.workspace.get_emb_sel_sfs(pt_1, eta_1, pt_2, self.etabinning),
                                         }
                                         for pt_2 in self.ptbinning[:-1]
                                     ],
@@ -377,13 +359,13 @@ class emb_doublemuon_correction(Correction):
             )
         else:
             raise Exception("Not implemented")
-        
+
         return sfs
 
-    def generate_scheme(self):
+    def generate_scheme(self, workspace_file=None):
         self.parse_config()
         self.setup_scheme()
-        self.set_workspace()
+        self.set_workspace(workspace_file)
         self.correctionset["data"] = self.generate_sfs()
         output_corr = schema.Correction.parse_obj(self.correctionset)
         self.correction = output_corr
@@ -399,19 +381,19 @@ class emb_doublemuon_correction(Correction):
 
 
 if __name__ == "__main__":
-
-    ROOT.PyConfig.IgnoreCommandLineOptions = True
-    ROOT.gROOT.SetBatch(ROOT.kTRUE)
-    # for keeping the histograms in memory
-    ROOT.TH1.AddDirectory(0)
-    test = pt_eta_correction(
-        tag="test",
-        name="EmbID_pt_eta_bins",
-        outdir="output/jsons",
-        configfile="settings/settings_embeddingselection_2018UL.yaml",
-        era="2018UL",
-        fname="{}/{}.json".format("output/jsons", "test"),
-        data_only=True,
-        verbose=False,
-    )
-    test.generate_scheme()
+    pass
+    # ROOT.PyConfig.IgnoreCommandLineOptions = True
+    # ROOT.gROOT.SetBatch(ROOT.kTRUE)
+    # # for keeping the histograms in memory
+    # ROOT.TH1.AddDirectory(0)
+    # test = pt_eta_correction(
+    #     tag="test",
+    #     name="EmbID_pt_eta_bins",
+    #     outdir="output/jsons",
+    #     configfile="settings/settings_embeddingselection_2018UL.yaml",
+    #     era="2018UL",
+    #     fname="{}/{}.json".format("output/jsons", "test"),
+    #     data_only=True,
+    #     verbose=False,
+    # )
+    # test.generate_scheme()
